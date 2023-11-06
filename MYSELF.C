6ebe0a1f6f69d94b17455738638c959828613403
taksh <taksh5834@gmail.com>
2023-11-06 18:58:52 +0530

    1.2

---
+++ MYSELF.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+clrscr();
+printf("My name is taksh.... \n");
+printf("My age 17.... \n");
+printf("My school name is p.p savani.... \n");
+printf("My courese name is flutter.... \n");
+printf("My gr id is 4358.... \n");
+printf("I am living in surat \n");
+getch();
+}
---
+++ PETTERN2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+clrscr();
+
+printf("*\n");
+printf("*\t*\n");
+printf("*\t*\t*\n");
+printf("*\t*\n");
+printf("*\n");
+
+
+getch();
+  }
